0934fbeb5d2be9ac98fc9e741b1fd587c5bdef7c
MrMariu5 <marius.sadreika@gmail.com>
2020-10-13 22:11:06 +0300

    Fixed

--- Norwegian/Norwegian/Program.cs
+++ Norwegian/Norwegian/Program.cs
@@ -5,13 +5,7 @@
         static void Main(string[] args)
         {
             Crawler crawler = new Crawler();
-            //Proxy proxy = new Proxy();
-            //List<Proxy> proxyAddressList = new List<Proxy>();
-            //proxyAddressList = proxy.hidemyProxy();
-            //foreach(Proxy prox in proxyAddressList)
-            {
-                crawler.crawling(/*prox*/);
-            }
+            crawler.crawling();
         }
     }
 }
