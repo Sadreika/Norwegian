d2cd716306b67ed31600ad3f10bf5cf8bcc225a6
MrMariu5 <marius.sadreika@gmail.com>
2020-10-15 09:20:34 +0300

    RewrittenOnHttpClient

--- Norwegian/Norwegian/Program.cs
+++ Norwegian/Norwegian/Program.cs
@@ -1,11 +1,14 @@
-﻿namespace Norwegian
+﻿using System;
+
+namespace Norwegian
 {
     class Program
     {
         static void Main(string[] args)
         {
             Crawler crawler = new Crawler();
-            crawler.crawling();
+            crawler.crawlingAsync().Wait();
+            Console.ReadLine();
         }
     }
 }
